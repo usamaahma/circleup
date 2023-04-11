0bf6814108f74d8d99c50fc59fddf76f1e6f8931
usamaahma <usamaahmad087@gmail.com>
2023-04-12 00:16:01 +0500

    header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
 import Landing from "./components/landing";
+import Header from "./components/main/header";
 
 function App() {
   return (
     <div className="App">
-      <Landing />
+      <Header />
     </div>
   );
 }
--- src/components/main/header.js
+++ src/components/main/header.js
@@ -1,7 +1,70 @@
 import React from "react";
+import "./header.css";
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import { Avatar } from "antd";
+
+const url =
+  "https://htmldemo.net/adda/adda/assets/images/profile/profile-midle-1.jpg";
 
 function Header() {
-  return <div>header</div>;
+  return (
+    <div>
+      <Container>
+        <Navbar bg="transparent" expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="#">
+              <img src="favicon.png" alt="abc" className="logo"></img>
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1" className="navbartxt">
+                  Home
+                </Nav.Link>
+                <Nav.Link href="#action2" className="navbartxt">
+                  Message
+                </Nav.Link>
+
+                <Nav.Link href="#action3" className="navbartxt">
+                  Notification
+                </Nav.Link>
+              </Nav>
+              <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px" }}
+                navbarScroll
+              >
+                <Nav.Link href="#action1">
+                  <img
+                    src="https://htmldemo.net/adda/adda/assets/images/logo/logo.png"
+                    alt="abc"
+                  ></img>
+                </Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+              <Avatar size={40} className="avatar" src={url} />
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </div>
+  );
 }
 
 export default Header;
