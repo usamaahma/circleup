06a5b797a05cdc4ab087998e4e25828151cb09a7
noman12311 <zishishah111@gmail.com>
2023-04-12 22:44:01 +0500

    done

---
+++ src/components/heroSection/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function HeroSection() {
+  return (
+    <div>
+      <div className="bg-image"></div>
+    </div>
+  );
+}
+
+export default HeroSection;
--- src/screens/landingPage/landing.js
+++ src/screens/landingPage/landing.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../../components/haeder/header";
+import HeroSection from "../../components/heroSection";
 
 function Landing() {
   return (
     <div>
       <Header />
+      <HeroSection />
     </div>
   );
 }
